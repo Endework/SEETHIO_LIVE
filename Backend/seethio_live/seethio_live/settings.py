53550aeb72b78f403ca4a1f14b4124315886e6ee
Austasty <obuesiogonna2@gmail.com>
2023-12-03 17:11:22 +0100

    updated requirements.tst files

--- Backend/seethio_live/seethio_live/settings.py
+++ Backend/seethio_live/seethio_live/settings.py
@@ -15,9 +15,12 @@ from pathlib import Path
 import os
 from decouple import config
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = [BASE_DIR / "static"] # new for render deploy
+STATIC_ROOT = BASE_DIR / "staticfiles" # new form render deploy
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -28,7 +31,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- Backend/seethio_live/seethio_live/urls.py
+++ Backend/seethio_live/seethio_live/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from userauthentication import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new from render deploy
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,3 +27,5 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path('auth/', include('social_django.urls', namespace='social')),  # Social authentication by ogo
 ]
+
+urlpatterns += staticfiles_urlpatterns() # new for render deploy
