7445c0832bc9a2af356b03dd38cea13be5f5abc5
Austasty <obuesiogonna2@gmail.com>
2023-12-03 18:50:00 +0100

    updated setthings

--- Backend/seethio_live/seethio_live/settings.py
+++ Backend/seethio_live/seethio_live/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from dotenv import load_dotenv
-load_dotenv()
 from pathlib import Path
 import os
 from decouple import config
 
+
+if Path(".env").exists():
+    load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
