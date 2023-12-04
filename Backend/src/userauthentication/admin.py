076eaa2a0349387bc3080dc310519d7e635ed7bb
Austasty <obuesiogonna2@gmail.com>
2023-12-04 17:47:34 +0100

    added

--- Backend/src/userauthentication/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Subscribers
-
-# Register your models here.
-class UserAdmin(admin.ModelAdmin):
-    list_display =['username', 'email']
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Subscribers)
